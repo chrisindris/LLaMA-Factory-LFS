8bc5997bcfd29943bf1ce0626952ceba0c9ab6c0
chrisindris <christopher.indris@gmail.com>
2025-11-17 12:14:06 -0500

    SQA3D: SFT working for 100 examples

--- preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
+++ preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=80   
-#SBATCH --time=0-00:25:00
-#SBATCH --gpus-per-node=h100:1
-#SBATCH --output=%N-qwen2_5vl_lora_sft-%j.out
+#SBATCH --cpus-per-task=96   
+#SBATCH --time=0-00:45:00
+#SBATCH --gpus-per-node=h100:4
+#SBATCH --output=%N-qwen2_5vl_lora_sft_SQA3D-%j.out
 
 module load apptainer
+
+TORCH_CUDA_ARCH_LIST="9.0" # for clusters with h100 GPUs
 
 # better to have triton cache on a non-nfs file system for speed
 # if we are offline, we need to indicate this
@@ -23,6 +25,10 @@ apptainer run --nv --writable-tmpfs \
     --env HF_HUB_CACHE="/scratch/indrisch/huggingface/hub" \
     --env TRITON_CACHE_DIR="${SLURM_TMPDIR}/.triton_cache" \
     --env FLASHINFER_WORKSPACE_BASE="/scratch/indrisch/" \
+    --env TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}" \
+    --env TORCH_EXTENSIONS_DIR="${SLURM_TMPDIR}/.cache/torch_extensions" \
+    --env PYTORCH_KERNEL_CACHE_PATH="${SLURM_TMPDIR}/.cache/torch/kernels" \
+    --env FORCE_TORCHRUN=1 \
     --pwd /scratch/indrisch/LLaMA-Factory \
     /scratch/indrisch/easyr1_verl_sif/llamafactory.sif \
-    llamafactory-cli train /scratch/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft.yaml
+    llamafactory-cli train /scratch/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft_SQA3D.yaml
--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -57,6 +57,7 @@ def _load_single_dataset(
     r"""Load a single dataset and aligns it to the standard format."""
     logger.info_rank0(f"Loading dataset {dataset_attr}...")
     data_path, data_name, data_dir, data_files = None, None, None, None
+    
     if dataset_attr.load_from in ["hf_hub", "ms_hub", "om_hub"]:
         data_path = dataset_attr.dataset_name
         data_name = dataset_attr.subset
