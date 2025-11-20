817baa3b78c1c014a500c989cfe4f242efee624d
chrisindris <christopher.indris@gmail.com>
2025-11-20 16:38:16 -0500

    SQA3D: 500 examples completed

--- preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
+++ preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=24   
-#SBATCH --time=0-00:10:00
-#SBATCH --gpus-per-node=h100:1
+#SBATCH --cpus-per-task=96
+#SBATCH --time=0-03:00:00
+#SBATCH --gpus-per-node=h100:4
 #SBATCH --output=%N-qwen2_5vl_lora_sft_SQA3D-%j.out
 
-# 100 examples (per_device_train_batch_size: 2) takes 25 minutes; used 14.49% of memory (108.97 of 751.95 GB)
-# 500 examples (per_device_train_batch_size: 2) takes about 120 minutes; used 22.57% of memory (169.68 of 751.95 GB)
+# 100 examples (per_device_train_batch_size: 2) takes 25 minutes; used 14.49% of CPU memory (108.97 of 751.95 GB), no GPU OOM errors
+# 500 examples (per_device_train_batch_size: 2) on a full node (96 CPUs, 4 GPUs, 32 preproc workers and 32 dataloader workers) took 1.75 hours; used 30.80% of CPU memory (231.57 of 751.95 GB)
 
 module load apptainer
 
