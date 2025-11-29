c13271e477d124ca9df083196c6e59377de13534
Christopher Indris <indrisch@tamia1.tamia.ecpia.ca>
2025-11-29 03:00:01 -0500

    WIP: TamIA setup

--- preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
+++ preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3D.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=256GB
 #SBATCH --time=0-00:10:00
-#SBATCH --mem=512GB
-#SBATCH --gpus-per-node=h100:1
-#SBATCH --output=%N-qwen2_5vl_lora_sft_SQA3D-%j.out
+#SBATCH --gpus-per-node=h100:4
+#SBATCH --output=tamia-qwen2_5vl_lora_sft_SQA3D-%j.out
 
 # 100 examples (per_device_train_batch_size: 2) takes 25 minutes; used 14.49% of CPU memory (108.97 of 751.95 GB), no GPU OOM errors
 # 500 examples (per_device_train_batch_size: 2) on a full node (96 CPUs, 4 GPUs, 32 preproc workers and 32 dataloader workers) took 1.75 hours; used 30.80% of CPU memory (231.57 of 751.95 GB)
@@ -35,5 +35,5 @@ apptainer run --nv --writable-tmpfs \
     --env PYTORCH_KERNEL_CACHE_PATH="${SLURM_TMPDIR}/.cache/torch/kernels" \
     --env FORCE_TORCHRUN=1 \
     --pwd /project/aip-wangcs/indrisch/LLaMA-Factory \
-    /scratch/indrisch/huggingface/hub/datasets--cvis-tmu--easyr1_verl_sif/snapshots/382a3b3e54a9fa9450c6c99dd83efaa2f0ca4a5a/llamafactory.sif \
+    /project/aip-wangcs/indrisch/huggingface/hub/datasets--cvis-tmu--easyr1_verl_sif/snapshots/382a3b3e54a9fa9450c6c99dd83efaa2f0ca4a5a/llamafactory.sif \
     llamafactory-cli train /project/aip-wangcs/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft_SQA3D.yaml
