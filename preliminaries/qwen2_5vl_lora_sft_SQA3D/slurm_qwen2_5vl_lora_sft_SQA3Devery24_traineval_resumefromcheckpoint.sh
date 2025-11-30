77d107f0eef5342bab62d4896a4184acf9048ad7
chrisindris <christopher.indris@gmail.com>
2025-11-30 05:05:57 -0500

    saving TamIA before merge

--- preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3Devery24_traineval_resumefromcheckpoint.sh
+++ preliminaries/qwen2_5vl_lora_sft_SQA3D/slurm_qwen2_5vl_lora_sft_SQA3Devery24_traineval_resumefromcheckpoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=96
+#SBATCH --cpus-per-task=48
 #SBATCH --time=0-00:10:00
+#SBATCH --mem=256GB
 #SBATCH --gpus-per-node=h100:4
 #SBATCH --output=%N-qwen2_5vl_lora_sft_SQA3Devery24_traineval_resumefromcheckpoint-%j.out
 
@@ -17,7 +18,7 @@ TORCH_CUDA_ARCH_LIST="9.0" # for clusters with h100 GPUs
 # better to have triton cache on a non-nfs file system for speed
 # if we are offline, we need to indicate this
 apptainer run --nv --writable-tmpfs \
-    -B /scratch/indrisch/LLaMA-Factory \
+    -B /project/aip-wangcs/indrisch/LLaMA-Factory \
     -B /home/indrisch \
     -B /dev/shm:/dev/shm \
     -B /etc/ssl/certs:/etc/ssl/certs:ro \
@@ -25,17 +26,17 @@ apptainer run --nv --writable-tmpfs \
     -W ${SLURM_TMPDIR} \
     --env HF_HUB_OFFLINE=1 \
     --env MPLCONFIGDIR="${SLURM_TMPDIR}/.config/matplotlib" \
-    --env HF_HOME="/scratch/indrisch/huggingface/hub" \
-    --env HF_HUB_CACHE="/scratch/indrisch/huggingface/hub" \
+    --env HF_HOME="/project/aip-wangcs/indrisch/huggingface/hub" \
+    --env HF_HUB_CACHE="/project/aip-wangcs/indrisch/huggingface/hub" \
     --env TRITON_CACHE_DIR="${SLURM_TMPDIR}/.triton_cache" \
-    --env FLASHINFER_WORKSPACE_BASE="/scratch/indrisch/" \
+    --env FLASHINFER_WORKSPACE_BASE="/scratch/i/indrisch/" \
     --env TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}" \
     --env TORCH_EXTENSIONS_DIR="${SLURM_TMPDIR}/.cache/torch_extensions" \
     --env PYTORCH_KERNEL_CACHE_PATH="${SLURM_TMPDIR}/.cache/torch/kernels" \
     --env FORCE_TORCHRUN=1 \
-    --pwd /scratch/indrisch/LLaMA-Factory \
-    /scratch/indrisch/easyr1_verl_sif/llamafactory.sif \
-    llamafactory-cli train /scratch/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft_SQA3Devery24_traineval_resumefromcheckpoint.yaml
+    --pwd /project/aip-wangcs/indrisch/LLaMA-Factory \
+    /project/aip-wangcs/indrisch/easyr1_verl_sif/llamafactory.sif \
+    llamafactory-cli train /project/aip-wangcs/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft_SQA3Devery24_traineval_resumefromcheckpoint.yaml
 
 
 # STEP 2: RUN THE EVALUATION
