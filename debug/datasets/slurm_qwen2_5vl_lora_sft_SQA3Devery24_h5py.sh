2242863d5733a39a6f7fe0b3eb2a02c14d22a3e2
chrisindris <christopher.indris@gmail.com>
2026-01-08 02:36:08 -0500

    llamafactory can use h5py-packed SQA3D or regular SQA3D

--- debug/datasets/slurm_qwen2_5vl_lora_sft_SQA3Devery24_h5py.sh
+++ debug/datasets/slurm_qwen2_5vl_lora_sft_SQA3Devery24_h5py.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+<<<<<<< HEAD
 
 # SBATCH directives - Default to RORQUAL settings
 # For TRILLIUM: Uncomment the TRILLIUM section below and comment out conflicting RORQUAL directives,
@@ -58,12 +59,26 @@ fi
 # if we are offline, we need to indicate this
 
 RUNNING_MODE=$1 # this is optional; generally, we wouldn't use this
+=======
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=495GB
+#SBATCH --gpus-per-node=l40s:4
+#SBATCH --output=out/SQA3D_h5py/%N-qwen2_5vl_lora_sft_SQA3Devery24_h5py-%j.out
+
+
+RUNNING_MODE=$1 # this is optional; generally, we wouldn't use this
+YAML_FILE="/project/aip-wangcs/indrisch/LLaMA-Factory/examples/train_lora/qwen2_5vl_lora_sft_SQA3Devery24_h5py.yaml"
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
 
 if [[ "$RUNNING_MODE" == "APPTAINER" ]]; then
 
     module load apptainer
 
     TORCH_CUDA_ARCH_LIST="9.0" # for clusters with h100 GPUs
+<<<<<<< HEAD
     
     # Build apptainer command with cluster-specific settings
     APPTAINER_CMD="apptainer run --nv --writable-tmpfs"
@@ -177,6 +192,35 @@ elif [[ "$RUNNING_MODE" == "SHELL" ]]; then
     #     /scratch/indrisch/huggingface/hub/datasets--cvis-tmu--easyr1_verl_sif/snapshots/382a3b3e54a9fa9450c6c99dd83efaa2f0ca4a5a/llamafactory.sif \
     #     bash
 
+=======
+
+    # STEP 1: RUN THE TRAINING AND EVALUATION
+
+    # better to have triton cache on a non-nfs file system for speed
+    # if we are offline, we need to indicate this
+    apptainer run --nv --writable-tmpfs \
+        -B /project/aip-wangcs/indrisch/LLaMA-Factory \
+        -B /home/indrisch \
+        -B /dev/shm:/dev/shm \
+        -B /etc/ssl/certs:/etc/ssl/certs:ro \
+        -B /etc/pki:/etc/pki:ro \
+        -W ${SLURM_TMPDIR} \
+        --env HF_HUB_OFFLINE=1 \
+        --env MPLCONFIGDIR="${SLURM_TMPDIR}/.config/matplotlib" \
+        --env HF_HOME="/scratch/indrisch/huggingface/hub" \
+        --env HF_HUB_CACHE="/scratch/indrisch/huggingface/hub" \
+        --env TRITON_CACHE_DIR="${SLURM_TMPDIR}/.triton_cache" \
+        --env FLASHINFER_WORKSPACE_BASE="/scratch/indrisch/" \
+        --env TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}" \
+        --env TORCH_EXTENSIONS_DIR="${SLURM_TMPDIR}/.cache/torch_extensions" \
+        --env PYTORCH_KERNEL_CACHE_PATH="${SLURM_TMPDIR}/.cache/torch/kernels" \
+        --env FORCE_TORCHRUN=1 \
+        --env WANDB_MODE=offline \
+        --env WANDB_DIR="/project/aip-wangcs/indrisch/LLaMA-Factory/wandb/" \
+        --pwd /project/aip-wangcs/indrisch/LLaMA-Factory \
+        /project/aip-wangcs/indrisch/easyr1_verl_sif/llamafactory.sif \
+        llamafactory-cli train ${YAML_FILE}
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
 
 elif [[ "$RUNNING_MODE" == "VENV" ]]; then
 
@@ -184,6 +228,7 @@ elif [[ "$RUNNING_MODE" == "VENV" ]]; then
     module load python/3.12 cuda/12.6 opencv/4.12.0
     module load arrow
 
+<<<<<<< HEAD
     source /scratch/indrisch/venv_llamafactory_cu126/bin/activate
 
     # pushd /scratch/indrisch/
@@ -193,6 +238,33 @@ elif [[ "$RUNNING_MODE" == "VENV" ]]; then
     # virtualenv --no-download venv_llamafactory_cu126
     # source venv_llamafactory_cu126/bin/activate
     # popd
+=======
+    export DS_BUILD_CPU_ADAM=1
+    export DS_BUILD_AIO=1
+    export DS_BUILD_UTILS=1
+    export LIBRARY_PATH="${CUDA_HOME}/lib64:$LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:$LD_LIBRARY_PATH"
+    # Enable AVX512 compilation flags for DeepSpeed CPU Adam extension
+    # DeepSpeed detects AVX512 and uses -D__AVX512__, but compiler needs -mavx512f to actually compile AVX512 code
+    export CXXFLAGS="-mavx512f ${CXXFLAGS:-}"
+    export CFLAGS="-mavx512f ${CFLAGS:-}"
+
+    #if /project/aip-wangcs/indrisch/venv_llamafactory_cu126/bin/activate exists, use it; otherwise, build from scratch
+    if [ -f /project/aip-wangcs/indrisch/venv_llamafactory_cu126/bin/activate ]; then
+        source /project/aip-wangcs/indrisch/venv_llamafactory_cu126/bin/activate
+    else
+        pushd /project/aip-wangcs/indrisch/
+        virtualenv --no-download venv_llamafactory_cu126
+        source venv_llamafactory_cu126/bin/activate
+        pushd /project/aip-wangcs/indrisch/LLaMA-Factory
+        pip install --upgrade pip setuptools wheel
+        # Note that using CPU ADAM doesn't seem to work on Killarney; hence, avoid using deepspeed versions which use offload.
+        # DS_BUILD_CPU_ADAM=1 DS_BUILD_AIO=1 DS_BUILD_UTILS=1 pip install -e ".[torch,metrics,deepspeed,liger-kernel]"
+        # DS_BUILD_CPU_ADAM=1 DS_BUILD_AIO=1 DS_BUILD_UTILS=1 pip install wandb ray
+        pip install -e ".[torch,metrics,deepspeed,liger-kernel]"
+        pip install wandb ray
+    fi
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
     
     # Set environment variables AFTER venv activation to ensure they persist
     # Create cache directories before they're needed
@@ -201,7 +273,10 @@ elif [[ "$RUNNING_MODE" == "VENV" ]]; then
     mkdir -p "${SLURM_TMPDIR}/.config/matplotlib"
     mkdir -p "${SLURM_TMPDIR}/.triton_cache"
     
+<<<<<<< HEAD
     export CUDA_VISIBLE_DEVICES=0,1,2,3
+=======
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
     export HF_HUB_OFFLINE=1 
     export MPLCONFIGDIR="${SLURM_TMPDIR}/.config/matplotlib"
     export HF_HOME="/scratch/indrisch/huggingface/hub"
@@ -213,6 +288,7 @@ elif [[ "$RUNNING_MODE" == "VENV" ]]; then
     export PYTORCH_KERNEL_CACHE_PATH="${SLURM_TMPDIR}/.cache/torch/kernels"
     export FORCE_TORCHRUN=1 
     export WANDB_MODE=offline 
+<<<<<<< HEAD
     export WANDB_DIR="/scratch/indrisch/LLaMA-Factory/wandb/" 
     export DISABLE_VERSION_CHECK=1 # since the automatic detector doesn't automatically see that transformers==4.57.1+computecanada is the same as transformers==4.57.1
     # giving the slow tokenizer a try: https://github.com/hiyouga/LLaMA-Factory/issues/8600#issuecomment-3227071979
@@ -271,8 +347,21 @@ elif [[ "$RUNNING_MODE" == "VENV" ]]; then
     #     --per_device_eval_batch_size 1 \
     #     --eval_strategy steps \
     #     --eval_steps 200
+=======
+    export WANDB_DIR="/project/aip-wangcs/indrisch/LLaMA-Factory/wandb/" 
+    export DISABLE_VERSION_CHECK=1 # since the automatic detector doesn't automatically see that transformers==4.57.1+computecanada is the same as transformers==4.57.1
+    # giving the slow tokenizer a try: https://github.com/hiyouga/LLaMA-Factory/issues/8600#issuecomment-3227071979
+
+
+    pushd /project/aip-wangcs/indrisch/LLaMA-Factory
+    llamafactory-cli train ${YAML_FILE}
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
 
 else
     echo "Invalid running mode: $RUNNING_MODE"
     exit 1
+<<<<<<< HEAD
 fi
+=======
+fi
+>>>>>>> 7a3149e4 (llamafactory can use h5py-packed SQA3D or regular SQA3D)
